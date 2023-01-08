4ef53637564ad54deb651634b81be1e8107804e5
jewonkang <zunejw@gmail.com>
2023-01-08 21:47:25 +0900

    Initial commit

---
+++ NaverDataLab.py
@@ -0,0 +1,44 @@
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import time
+
+#오늘 날짜가 며칠인지 점검하는 함수
+def get_today():
+    today = time.strftime('%Y.%m.%d', time.localtime(time.time()))
+    return today[-2:]
+
+def get_keywords():
+    #오늘 날짜를 우선 점검함 : 01, 11 , 21일때만 상품리스트를 가져오게
+    if get_today() == '08' or get_today() == '11' or get_today() == '21':
+    #창 열기 쇼핑인사이트
+        chrome_options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()), options = chrome_options )
+        driver.get(url = 'https://datalab.naver.com/shoppingInsight/sCategory.naver')
+    #카테고리 = 디지털가전
+        category = '//*[@id="content"]/div[2]/div/div[1]/div/div/div[1]/div/div[1]/ul/li[4]/a'
+        Keywords = []
+        driver.find_element(By.XPATH, '//*[@id="content"]/div[2]/div/div[1]/div/div/div[1]/div/div[1]/span').click()
+        driver.find_element(By.XPATH, category).click()
+        time.sleep(0.5)
+        driver.find_element(By.XPATH, '//*[@id="18_device_0"]').click()
+        driver.find_element(By.XPATH, '//*[@id="19_gender_0"]').click()
+        driver.find_element(By.XPATH, '//*[@id="20_age_0"]').click()
+        driver.find_element(By.XPATH, '//*[@id="content"]/div[2]/div/div[1]/div/a').click()
+
+        for j in range(1, 6):
+            path = f'//*[@id="content"]/div[2]/div/div[2]/div[2]/div/div/div[1]/ul/li[{j}]/a'
+            time.sleep(0.1)
+            result = driver.find_element(By.XPATH, path).text
+
+            Keywords.append(result.split('\n')[1])
+            time.sleep(1)
+        print(Keywords)
+        driver.close()
+        return Keywords
+    else :
+        return None
+
+
+print(get_keywords())
---
+++ posting.py
@@ -0,0 +1,51 @@
+import sys
+import os
+import pickle
+from oauth2client import client
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+BLOG_ID = "8038814675849111994" #구글블로거아이디
+
+SCOPES = ['https://www.googleapis.com/auth/blogger']
+
+def get_blogger_service_obj() :
+    creds = None
+    if os.path.exists('auto_token.pickle') :
+        with open('auto_token.pickle','rb') as token :
+            creds = pickle.load(token)
+    if not creds or not creds.valid :
+        if creds and creds.expired and creds.refresh_token :
+            creds.refresh(Request())
+        else :
+            # 다운받은 json이 있는 주소를 적어주기
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'C:/users/Lenovo/PycharmProjects/autoposting/client_secret.json', SCOPES)
+            creds = flow.run_local_server(port = 0)
+        with open('auto_token.pickle','wb') as token :
+            pickle.dump(creds, token)
+    blog_service = build('blogger','v3', credentials = creds)
+    drive_service = build('drive','v3',credentials = creds)
+    return drive_service, blog_service
+
+drive_handler, blog_handler = get_blogger_service_obj()
+
+keyword = '자동포스팅' #태그
+blogger_title = '오 감동적이다' #블로그제목
+content = '이야 이게 되네?' #내용
+
+data = {
+    'content':content,
+    'title' : blogger_title,
+    'labels' : keyword,
+    'blog' : {
+        'id' : BLOG_ID,
+    },}
+
+posts = blog_handler.posts()
+
+res = posts.insert(blogId = BLOG_ID, body = data, isDraft = False, fetchImages = True).execute()
+res #결과물
+
+
